2449c3a77834a7025c033652a4081d5694692f5c
Green-Shrek55 <113928010+Green-Shrek55@users.noreply.github.com>
2024-02-08 13:10:25 +0300

    Add files via upload

---
+++ Praca3/Praca3/Praca3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+double SinRoutR(double inR, double outR) {
+    const float PI = 3.14;
+    double S = PI * (outR * outR - inR * inR);
+    return S;
+}
+int main() {
+    setlocale(LC_ALL, "Rus");
+    double inR, outR;
+    cout << "Введведите внешний радиус: ";
+    cin >> outR;
+    cout << "Введите внутренний: ";
+    cin >> inR;
+
+    double S = SinRoutR(inR, outR);
+    cout << "Площадь кольца: " << S << endl;
+
+    return 0;
+}
